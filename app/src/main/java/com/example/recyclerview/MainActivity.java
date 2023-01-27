b11552e9fd992c06b9ee78a55fd2c7c401cdbf43
Monarquet <monarco.199@gmail.com>
2023-01-27 17:50:17 +0100

    Terminado

--- app/src/main/java/com/example/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/recyclerview/MainActivity.java
@@ -1,28 +1,40 @@
 package com.example.recyclerview;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.recycleview.databinding.ActivityMainBinding;
+
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private WordListAdapter mAdapter;
-    private LinkedList<String> mWordList;
+    private LinkedList<String> mWorldList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        mRecyclerView = findViewById(R.id.recyclerView);
-        mAdapter = new WordListAdapter(this, mWordList);
+        mWorldList = new LinkedList<>();
+        WordListAdapter mAdapter = new WordListAdapter(this, mWorldList);
+
+        RecyclerView mRecyclerView = binding.recyclerview;
         mRecyclerView.setAdapter(mAdapter);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        binding.fab.setOnClickListener(v -> {
+            String newElement = "Word " + (mWorldList.size()+1);
+            mWorldList.add(newElement);
+            mRecyclerView.getAdapter().notifyItemChanged(mWorldList.size());
+            mRecyclerView.smoothScrollToPosition(mWorldList.size());
+        });
+
     }
 }
--- app/src/main/java/com/example/recyclerview/WordListAdapter.java
+++ app/src/main/java/com/example/recyclerview/WordListAdapter.java
@@ -1,6 +1,5 @@
 package com.example.recyclerview;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,47 +9,29 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.recycleview.R;
+
 import java.util.LinkedList;
 
 public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
 
-    private final LayoutInflater mInflater;
-    private final LinkedList<String> mWordList;
+    private LayoutInflater mInfalter;
+    private LinkedList<String> mWordList;
 
     public WordListAdapter(Context context, LinkedList<String> wordList) {
-        mInflater = LayoutInflater.from(context);
+        mInfalter = LayoutInflater.from(context);
         this.mWordList = wordList;
     }
 
-    class WordViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener {
-
-        private final WordListAdapter mAdapter;
-        TextView wordItemView;
-
-        public WordViewHolder(View itemView, WordListAdapter adapter) {
-            super(itemView);
-            wordItemView = itemView.findViewById(R.id.word);
-            this.mAdapter = adapter;
-            itemView.setOnClickListener(this);
-        }
-
-        @SuppressLint("SetTextI18n")
-        @Override
-        public void onClick(View v) {
-
-            wordItemView.setText ("Clicked! "+ wordItemView.getText());
-        }
+    @NonNull
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInfalter.inflate(R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView);
     }
 
     @Override
-    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) { // Inflate an item
-
-        View mItemView = mInflater.inflate(R.layout.item_text_view, parent, false);
-        return new WordViewHolder(mItemView, this);
-    }
-
-    @Override
-    public void onBindViewHolder(WordViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
         String mCurrent = mWordList.get(position);
         holder.wordItemView.setText(mCurrent);
     }
@@ -58,5 +39,21 @@ public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordVi
     @Override
     public int getItemCount() {
         return mWordList.size();
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        TextView wordItemView;
+
+        public WordViewHolder(@NonNull View itemView) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.word);
+            itemView.setOnClickListener(this::onClick);
+        }
+
+        @Override
+        public void onClick(View v) {
+            wordItemView.setText("Clicked! " + wordItemView.getText());
+        }
     }
 }
